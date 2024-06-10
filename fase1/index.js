0fe08859ff5866ad7d560ce3e413738065770664
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2024-06-10 16:55:01 -0600

    borrar try catch

--- fase1/index.js
+++ fase1/index.js
@@ -4,13 +4,7 @@ document.getElementById("x").addEventListener("keydown", function(e) {
     if (e.key === "Enter") {
         x = document.getElementById("x").value;
         var container = document.getElementById("mynetwork");
-        try {
-            var result = PEG.parse(x);
-            console.log(result);
-        } catch (e) {
-            console.log(e.message); // Mensaje de error
-            console.log(`Error en línea ${e.location.start.line}, columna ${e.location.start.column}, offset ${e.location.start.offset}`);
-            console.log(`Carácter no válido: ${e.invalidChar}`);
-        }
+        var result = PEG.parse(x);
+        console.log(result);
     }
 });
