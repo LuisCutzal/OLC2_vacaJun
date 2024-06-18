efc7986b942b28b7b2b6c18dd89775be93cc066a
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2024-06-17 21:31:39 -0600

    fase2
    changes f2

---
+++ fase2/script.js
@@ -0,0 +1,290 @@
+let errorTable, symbolTable, Arm64Editor, consoleResult, dotStringCst = "", currentStr = "", Arm64Editors = [];
+
+$(document).ready(function () {
+    addTab();
+    consoleResult = editor('console_log', '', true, true, false);
+});
+
+
+function addTab() {
+    // Obtener el listado de las pestañas actuales
+    let btns = document.querySelectorAll(".LEditor .textEditor .buttonTab");
+    let id = btns.length;
+
+    // crear nuevo botón de pestaña
+    let btn = document.createElement('button');
+    btn.textContent = "T" + id;
+    btn.className = "buttonTab";
+
+    document.getElementById('buttonContainer').appendChild(btn);
+
+    btn.addEventListener('click', () => showSelectedTab(id));
+
+    // Crear nueva área de texto para la pestaña
+    let area = document.createElement('textarea');
+    area.className = "textTab";
+    area.id = id;
+    document.getElementById('tabs').appendChild(area);
+
+    // creando el nuevo code mirror
+    Arm64Editors.push(editor(id, 'text/x-rustsrc'));
+
+    showSelectedTab(id);
+}
+
+
+function showSelectedTab(id) {
+    //Actualizar el editor del que se extraerá el texto para el análisis
+    Arm64Editor = Arm64Editors[id];
+
+    //mostrar todas las pestañas
+    let btns = document.querySelectorAll(".LEditor .textEditor .buttonTab");
+
+    btns.forEach(function (node) {
+        node.style.backgroundColor = "gray";
+        node.style.color = "white";
+        node.style.fontFamily = "Helvetica, Sans-serif";
+        node.style.borderRadius = "2px 2px 0 0";
+        node.style.border = "2px";
+        node.style.padding = "2px 4px";
+
+    });
+
+    // resaltar la pestaña seleccionada
+    btns[id].style.backgroundColor = "#FF5722";
+
+    //ocultar las todas las áreas de texto
+    let codeMirrors = document.querySelectorAll('.CodeMirror');
+
+    for (let i = 0; i < codeMirrors.length - 1; i++) {
+        codeMirrors[i].style.display = 'none';
+    }
+
+    //mostrar solo el área de texto seleccionado
+    codeMirrors[id].style.display = 'block';
+
+}
+
+
+function editor(id, language, lineNumbers = true, readOnly = false, styleActiveLine = true) {
+    return CodeMirror.fromTextArea(document.getElementById(id), {
+        lineNumbers: lineNumbers,
+        readOnly: readOnly,
+        styleActiveLine: styleActiveLine,
+        matchBrackets: true,
+        theme: "moxer",
+        mode: language,
+        id: id
+
+    });
+}
+
+function cleanEditors() {
+    Arm64Editor.setValue("");
+    consoleResult.setValue("");
+    cleanErrorsTable(); // Limpiar la tabla de errores
+}
+
+function openFileDialog() {
+    var input = document.createElement("input");
+    input.type = "file";
+    input.click();
+
+    input.addEventListener("change", function (event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            var fileContent = e.target.result;
+            Arm64Editor.setValue(fileContent);
+        };
+
+        reader.onerror = (e) => {
+            let result = "No se pudo leer el archivo: " + " " + e.target.error;
+            consoleResult.setValue(result);
+        };
+
+        reader.readAsText(file);
+    });
+}
+
+const saveFile = async (fileName, extension, editor) => {
+    if (!fileName) {
+        const { value: name } = await Swal.fire({
+            title: 'Enter File name',
+            input: 'text',
+            inputLabel: 'File name',
+            showCancelButton: true,
+            inputValidator: (value) => {
+                if (!value) {
+                    return 'You need to write something!';
+                }
+            }
+        });
+        fileName = name;
+    }
+    if (fileName) {
+        download(`${fileName}.${extension}`, editor.getValue());
+    }
+};
+
+const download = (name, content) => {
+    let blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
+    let link = document.getElementById('download');
+    link.href = URL.createObjectURL(blob);
+    link.setAttribute("download", name);
+    link.click();
+};
+
+function isLexicalError(e) {
+    const validIdentifier = /^[a-zA-Z_$][a-zA-Z0-9_$]*$/;
+    const validInteger = /^[0-9]+$/;
+    const validRegister = /^[a-zA-Z][0-9]+$/;
+    const validCharacter = /^[a-zA-Z0-9_$,\[\]#"]$/;
+    if (e.found) {
+        if (!validIdentifier.test(e.found) &&
+            !validInteger.test(e.found) &&
+            !validRegister.test(e.found) &&
+            !validCharacter.test(e.found)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+let errorCounter = 0;
+
+const analysis = async () => {
+    const text = Arm64Editor.getValue();
+    //const text = currentStr;
+    cleanErrorsTable();
+    errorCounter = 0;
+    try {
+        let resultado = PEG.parse(text);
+        generateCST(resultado);
+        console.log(resultado);
+        if (resultado.errors.length > 0) {
+            consoleResult.setValue("Error, ver tabla de errores");
+            resultado.errors.forEach(error => {
+                const errorType = error.message.includes("Unrecognized input") ? 'Sintáctico' : 'Lexico';
+                console.log(error.message.includes("Unrecognized input"))
+                const errorMessage = error.message;
+                const errorLocation = `Fila: ${error.location.start.line}, Columna: ${error.location.start.column}`;
+
+                addErrorToTable(errorType, error.location.start.line, error.location.start.column, errorMessage);
+            });
+        } else {
+            consoleResult.setValue("VALIDO");
+        }
+    } catch (e) {
+        if (e instanceof PEG.SyntaxError) {
+            const errorType = 'Sintáctico';
+            const errorMessage = e.message;
+            const errorLocation = `Fila: ${e.location.start.line}, Columna: ${e.location.start.column}`;
+
+            consoleResult.setValue(`Error, ver tabla de errores`);
+            console.log(errorMessage);
+
+            addErrorToTable(errorType, e.location.start.line, e.location.start.column, errorMessage);
+        } else {
+            console.error('Error desconocido:', e);
+        }
+    }
+
+
+
+};
+
+function isLexicalError(error) {
+    return error.location.start.line === 1;
+}
+
+function cleanErrorsTable() {
+    const table = document.getElementById('errorsTable');
+    while (table.rows.length > 1) {
+        table.deleteRow(1);
+    }
+}
+
+function addErrorToTable(type, line, column, message) {
+    const table = document.getElementById('errorsTable');
+    const row = table.insertRow();
+    row.style.backgroundColor = 'white';
+    row.style.color = 'black';
+
+    const cellNumber = row.insertCell(0);
+    const cellType = row.insertCell(1);
+    const cellLine = row.insertCell(2);
+    const cellColumn = row.insertCell(3);
+    const cellMessage = row.insertCell(4);
+
+    cellNumber.textContent = ++errorCounter;
+    cellNumber.style.border = '1px solid black';
+    cellNumber.style.padding = '8px';
+
+    cellType.textContent = type;
+    cellType.style.border = '1px solid black';
+    cellType.style.padding = '8px';
+
+    cellLine.textContent = line;
+    cellLine.style.border = '1px solid black';
+    cellLine.style.padding = '8px';
+
+    cellColumn.textContent = column;
+    cellColumn.style.border = '1px solid black';
+    cellColumn.style.padding = '8px';
+
+    cellMessage.textContent = message;
+    cellMessage.style.border = '1px solid black';
+    cellMessage.style.padding = '8px';
+}
+
+
+
+const btnAnalysis = document.getElementById('run');
+btnAnalysis.addEventListener('click', () => {
+    analysis();
+
+    let codeMirrors = document.querySelectorAll('.CodeMirror');
+
+    // obtener el console log y añadirle animación cada que muestre un resultado
+    codeMirrors[codeMirrors.length - 1].style.animation = "none";
+    codeMirrors[codeMirrors.length - 1].offsetHeight;
+    codeMirrors[codeMirrors.length - 1].style.animation = "rainbow 0.5s";
+
+
+});
+
+const link2 = document.getElementById('download');
+link2.addEventListener('click', () => {
+    saveFile("file", "s", Arm64Editor);
+});
+//-----------------------------------------------------------------------------------------
+//-----------------------------------fase 2------------------------------------------------
+//-----------------------------------------------------------------------------------------
+
+function generateCST(DOTstring){
+    var container = document.getElementById("cst");
+    //var DOTstring = PEG.parse(x);
+    var parsedData = vis.parseDOTNetwork(DOTstring);
+    var data = {
+        nodes: parsedData.nodes,
+        edges: parsedData.edges
+    }
+    var options = {
+        nodes: {
+            widthConstraint: 20,
+        },        
+        layout: {
+            hierarchical: {
+                levelSeparation: 60,
+                nodeSpacing: 80,
+                parentCentralization: true,
+                direction: 'UD',        // UD, DU, LR, RL
+                sortMethod: 'directed',  // hubsize, directed
+                shakeTowards: 'roots'  // roots, leaves                        
+            },
+        },                        
+    };
+    var network = new vis.Network(container, data, options);
+};
