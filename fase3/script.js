1504d39cdd992e0ddb0ad1bc5d17cb55a0a67668
Pedro <pedromartinf07@gmail.com>
2024-06-26 22:40:03 -0600

    Refactor code to use ES6 modules

--- fase3/script.js
+++ fase3/script.js
@@ -1,4 +1,4 @@
-
+import {parse, SyntaxError} from './parser.js';
 
 let errorTable, symbolTable, Arm64Editor, consoleResult, dotStringCst = "", currentStr = "", Arm64Editors = [], quads = [];
 
@@ -185,19 +185,18 @@ function isLexicalError(e) {
 let errorCounter = 0;
 
 const analysis = async () => {
-    startTime = performance.now(); // Guardar tiempo de inicio
+    let startTime = performance.now(); // Guardar tiempo de inicio
     const text = Arm64Editor.getValue();
     //const text = currentStr;
     cleanErrorsTable();
     cleanQuadsTable();
     errorCounter = 0;
     try {
-        let resultado = PEG.parse(text);
+        let resultado = parse(text);
+        console.log(resultado);
         generateCST(resultado.getDot(resultado));
         generateQuads(resultado);
-        addQuadsToTable();
-
-
+        addQuadsToTable()
 
         /*if (resultado.errors.length > 0) {
             consoleResult.setValue("Error, ver tabla de errores");
@@ -213,7 +212,7 @@ const analysis = async () => {
             consoleResult.setValue("VALIDO");
         }*/
     } catch (e) {
-        if (e instanceof PEG.SyntaxError) {
+        if (e instanceof SyntaxError) {
             const errorType = 'Sintáctico';
             const errorMessage = e.message;
             const errorLocation = `Fila: ${e.location.start.line}, Columna: ${e.location.start.column}`;
@@ -227,7 +226,7 @@ const analysis = async () => {
         }
     }
 
-    endTime = performance.now();
+    let endTime = performance.now();
     const elapsedTime = (endTime - startTime).toFixed(3);
     // Mostrar el tiempo transcurrido en un elemento del DOM
     const tiempoTranscurridoElement = document.getElementById('tiempoTranscurrido');
@@ -242,9 +241,6 @@ const analysis = async () => {
 
 };
 
-function isLexicalError(error) {
-    return error.location.start.line === 1;
-}
 
 function cleanErrorsTable() {
     const table = document.getElementById('errorsTable');
