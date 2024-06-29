1da369d3bf21bafa5caff38ccc051bcbc7cba337
Pedro <pedromartinf07@gmail.com>
2024-06-29 09:39:41 -0600

    consts

---
+++ fase3/modules/const.js
@@ -0,0 +1,18 @@
+export function createCode(op){
+    let op = op.toUpperCase()
+    if (op==='MOV') return 1
+    if (op==='ADD') return 2
+    if (op==='SUB') return 3
+    if (op==='MUL') return 4
+    if (op==='DIV') return 5
+    if (op==='MOD') return 6
+
+}
+
+export const MOV = 1
+export const ADD = 2
+export const SUB = 3
+export const MUL = 4
+export const DIV = 5
+
+
