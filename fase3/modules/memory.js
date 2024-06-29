09da59a30b945b4694879f9ec12ffac7bff1abfd
Pedro <pedromartinf07@gmail.com>
2024-06-29 14:43:04 -0600

    principal memory

---
+++ fase3/modules/memory.js
@@ -0,0 +1,26 @@
+let memory = []
+
+//init memory with size
+function initMemory(size) {
+    if (size <= 0) {
+        throw new Error('Memory size must be greater than 0')
+    }
+    memory = new Array(size).fill(0)
+}
+
+//get memory
+function getMemory() {
+    return memory
+}
+
+//set memory at address
+function setMemory(address, value) {
+    if (address < 0 || address >= memory.length) {
+        throw new Error('Invalid memory address')
+    }
+    memory[address] = value
+}
+
+export default {initMemory, getMemory, setMemory}
+
+
