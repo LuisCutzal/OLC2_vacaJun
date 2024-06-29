fbe86a4d4e660c5505242696178f70f3bd0cef71
Neidy22 <3236875710511@ingenieria.usac.edu.gt>
2024-06-29 09:02:13 -0600

    Module quad

--- fase3/index.js
+++ fase3/index.js
@@ -1,7 +1,8 @@
-import {parse, StartRules, SyntaxError } from './parser/parser.js'
-import {openFileDialog} from './views/openFile.js'
+import { parse, StartRules, SyntaxError } from './parser/parser.js'
+import { openFileDialog } from './views/openFile.js'
+import { quads, generateQuads } from './modules/quad.js'
 
-let errorTable, symbolTable, Arm64Editor, consoleResult, dotStringCst = "", currentStr = "", Arm64Editors = [], quads = [];
+let errorTable, symbolTable, Arm64Editor, consoleResult, dotStringCst = "", currentStr = "", Arm64Editors = [];
 
 $(document).ready(function () {
     addTab();
@@ -322,7 +323,7 @@ function generateCST(DOTstring) {
                 //shakeTowards: 'roots'  // roots, leaves                        
             },
         },
-        edges: { 
+        edges: {
             arrows: "to",
         }
     };
@@ -330,62 +331,7 @@ function generateCST(DOTstring) {
 };
 
 
-function generateQuads(result) {
 
-    if (result.children.length > 0) {
-        result.children.forEach(function (element) {
-            //console.log(element);
-            switch (element.type) {
-                case "INSTRUCTION": // crear un nuevo cuadruplo por cada instrucción
-                case "SECTION": // crear un nuevo cuadruplo por cada sección
-                    let quad = new Quadruple();
-                    quad.setOperator(element.value);
-                    quads.push(quad);
-
-                    switch (element.value) {
-                        case 'SVC':
-                            quads[quads.length - 1].setArg1(element.children[0].value);
-                            break;
-                        case 'Section':
-                            quads[quads.length - 1].setResult(element.children[0].value);
-                            break;
-                    }
-
-                    break;
-
-                case "DESTINATION": // Asignar el valor del resultado del cuadruplo
-                    quads[quads.length - 1].setResult(element.children[0].value);
-                    break;
-                case "SOURCE1": // Asignar el valor del ARG1 del cuadruplo
-                    quads[quads.length - 1].setArg1(element.children[0].value);
-                    break;
-                case "SOURCE2": // Asignar el valor del ARG2 del cuadruplo
-                    quads[quads.length - 1].setArg2(element.children[0].value);
-                    break;
-                case "SOURCE3": // Asignar el valor del ARG3 del cuadruplo
-                    quads[quads.length - 1].setArg3(element.children[0].value);
-                    break;
-                case "SOURCE4": // Asignar el valor del ARG4 del cuadruplo
-                    quads[quads.length - 1].setArg4(element.children[0].value);
-                    break;
-                case "LABEL":
-                    // Verificar si el label corresponde a una etiqueta de salto y asignarla al resultado
-                    if (element.value === 'LABEL') quads[quads.length - 1].setResult(element.children[0].value);
-                    break;
-                /*case "DIRECTIVE":
-                    console.log(quads)
-                    quads[quads.length - 1].setOperator(element.value);
-                    quads[quads.length - 1].setArg1(element.children[0].value);
-                    break;*/
-
-            }
-
-            generateQuads(element); // llamada recursiva para ir a evaluar todos los hijos del nodo actual
-
-        });
-    }
-
-}
 
 function cleanQuadsTable() {
     const table = document.getElementById('quadsTable');
--- fase3/modules/quad.js
+++ fase3/modules/quad.js
@@ -68,3 +68,61 @@ class Quadruple {
     }
 
 }
+
+export let quads = [];
+export function generateQuads(result) {
+
+    if (result.children.length > 0) {
+        result.children.forEach(function (element) {
+            //console.log(element);
+            switch (element.type) {
+                case "INSTRUCTION": // crear un nuevo cuadruplo por cada instrucción
+                case "SECTION": // crear un nuevo cuadruplo por cada sección
+                    let quad = new Quadruple();
+                    quad.setOperator(element.value);
+                    quads.push(quad);
+
+                    switch (element.value) {
+                        case 'SVC':
+                            quads[quads.length - 1].setArg1(element.children[0].value);
+                            break;
+                        case 'Section':
+                            quads[quads.length - 1].setResult(element.children[0].value);
+                            break;
+                    }
+
+                    break;
+
+                case "DESTINATION": // Asignar el valor del resultado del cuadruplo
+                    quads[quads.length - 1].setResult(element.children[0].value);
+                    break;
+                case "SOURCE1": // Asignar el valor del ARG1 del cuadruplo
+                    quads[quads.length - 1].setArg1(element.children[0].value);
+                    break;
+                case "SOURCE2": // Asignar el valor del ARG2 del cuadruplo
+                    quads[quads.length - 1].setArg2(element.children[0].value);
+                    break;
+                case "SOURCE3": // Asignar el valor del ARG3 del cuadruplo
+                    quads[quads.length - 1].setArg3(element.children[0].value);
+                    break;
+                case "SOURCE4": // Asignar el valor del ARG4 del cuadruplo
+                    quads[quads.length - 1].setArg4(element.children[0].value);
+                    break;
+                case "LABEL":
+                    // Verificar si el label corresponde a una etiqueta de salto y asignarla al resultado
+                    if (element.value === 'LABEL') quads[quads.length - 1].setResult(element.children[0].value);
+                    break;
+                /*case "DIRECTIVE":
+                    console.log(quads)
+                    quads[quads.length - 1].setOperator(element.value);
+                    quads[quads.length - 1].setArg1(element.children[0].value);
+                    break;*/
+
+            }
+
+            generateQuads(element); // llamada recursiva para ir a evaluar todos los hijos del nodo actual
+
+        });
+    }
+
+}
