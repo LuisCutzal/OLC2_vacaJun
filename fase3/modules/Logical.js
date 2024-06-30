1f8129bb7d1b30af4783da88f0ebb84c433d0d72
Pedro <pedromartinf07@gmail.com>
2024-06-30 11:41:28 -0600

    update mov ins

--- fase3/modules/Logical.js
+++ fase3/modules/Logical.js
@@ -1,4 +1,5 @@
 import *  as cons from './const.js';
+import  {parseNum, typeOfArg } from './utilitis.js';
 
 class Logical{
     constructor(instruction){
@@ -14,8 +15,13 @@ class Logical{
             console.log("Instruction is undefined");
             return;
         }
-        if(this.instruction.opCode === cons.MOV){            
-            let value = parseInt(this.instruction.arg1);
+        if(this.instruction.opCode === cons.MOV){ 
+            let value = 0;    
+            if(typeOfArg(this.instruction.arg1) === cons.REG){
+                value = parseInt(registers.getRegister(this.instruction.arg1));
+            }if (typeOfArg(this.instruction.arg1) === cons.NUM || typeOfArg(this.instruction.arg1) === cons.D_NUM){
+                value = parseNum(this.instruction.arg1);
+            }
             registers.setRegister(this.instruction.res, parseInt(value));
         }
     }
