6626739b313c4023b8ee2a82d050858db2b51afc
Pedro <pedromartinf07@gmail.com>
2024-06-30 00:31:40 -0600

    update index

--- fase3/index.js
+++ fase3/index.js
@@ -2,13 +2,12 @@ import { parse, StartRules, SyntaxError } from './parser/parser.js'
 import { openFileDialog } from './views/openFile.js'
 import { quads, generateQuads } from './modules/quad.js'
 import { Registers, specialRegisters } from './modules/registers.js'
-import CPU from './controllers/CPU.js'
+import { CPU } from './controllers/CPU.js'
 import { Memory, Stack } from './modules/memory.js'
 
 
 let errorTable, symbolTable, Arm64Editor, consoleResult, dotStringCst = "", currentStr = "", Arm64Editors = [];
 const regs = new Registers(), specialRegs = new specialRegisters(), memory = new Memory(1024);
-const mv = new CPU();
 
 $(document).ready(function () {
     addTab();
