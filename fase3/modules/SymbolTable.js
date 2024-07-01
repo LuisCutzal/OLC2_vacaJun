b4657c5137d99a4999aac8d8fbe60a822faf80e7
Pedro <pedromartinf07@gmail.com>
2024-06-30 19:03:59 -0600

    symbol table

---
+++ fase3/modules/SymbolTable.js
@@ -0,0 +1,17 @@
+
+class SymbolTable{
+    constructor(){
+        this.table = {}
+    }
+
+    setSymbol(symbol, address, length){
+        this.table[symbol] = {address, length}
+    }
+
+    getSymbol(symbol){
+        if(!(symbol in this.table)){
+            throw new Error(`Symbol ${symbol} not found`)
+        }
+        return this.table[symbol]
+    }
+}
