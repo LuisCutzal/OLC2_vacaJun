3908ac2dc33c12619c71714c9edf10bdbb0e8ef9
Pedro <pedromartinf07@gmail.com>
2024-07-01 11:42:48 -0600

    update symbol table and memory

--- fase3/modules/DataAndDeclaration.js
+++ fase3/modules/DataAndDeclaration.js
@@ -7,24 +7,52 @@ class DataAndDeclaration {
     }
 
     run(memory, symbolTable) {
-        let count = 0;
-        console.log(this.instructions)
-        while (count < this.instructions.length) {
-            let op = this.instructions[count];
-            if (op.opCode === c.DIRECTIVE && op.arg1 !== '-' && op.res === '.ascii' ) {
-                let address = memory.allocate(op.arg1.length)
-                memory.setString(address, op.arg1)
-                
-                console.log(address)  
+        let count = 0
+        let currentSection = ''
+        this.instructions.forEach(({ opCode, op, arg1, arg2, arg3, res }) => {
+            count++
+            if (opCode === c.DIRECTIVE) {
+                switch (res.toLowerCase()) {
+                    case '.global':
+                        break
+                    case '.data':
+                        currentSection = 'data'
+                        break
+                    case '.bss':
+                        currentSection = 'bss'
+                        break
+                    case '.text':
+                        currentSection = 'text'
+                        break
+                    case '.asciz':
+                        const adrressAsciz = memory.allocate(arg1.length + 1)
+                        memory.setString(adrressAsciz, arg1)
+                        symbolTable.setSymbol(arg1, adrressAsciz, arg1.length + 1)
+                        break
+                    case '.ascii':
+                        const adrressAscii = memory.allocate(arg1.length + 1)
+                        memory.setString(adrressAscii, arg1)
+                        symbolTable.setSymbol(arg1, adrressAscii, arg1.length + 1)
+                        break
+                    case '.space':
+                        const spaceSize = parseInt(arg1, 10)
+                        const addressSpace = memory.allocate(spaceSize)
+                        memory.set(addressSpace, spaceSize)
+                        symbolTable.setSymbol(arg1, addressSpace, spaceSize)
+                        break
+                    default:
+                        console.warn(`Unknown directive: ${res}`)
+                        break    
+
+                }
+            }else if (opCode === c.SECTION){
+                symbolTable.setSymbol(res, count, 0)
             }
-            if (op.opCode == c.SECTION) {
-                symbolTable.setSymbol()
-            }
-            count += 1
-        }
-        console.log(memory)
-        console.log(memory.getString(0))
+        })
+        // console.log(memory.toHex())
+        console.log(symbolTable.table)
     }
+    
 }
 
 export { DataAndDeclaration }
