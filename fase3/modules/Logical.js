a2fa04cab9d31d13547c8883834034f0d8d15b77
Pedro <pedromartinf07@gmail.com>
2024-07-02 19:16:47 -0600

    remove parse int in mov options

--- fase3/modules/Logical.js
+++ fase3/modules/Logical.js
@@ -19,21 +19,21 @@ class Logical {
         if (this.instruction.opCode === cons.MOV) {
             let value = 0;
             if (typeOfArg(this.instruction.arg1) === cons.REG) {
-                value = parseInt(registers.getRegister(this.instruction.arg1))
+                value = registers.getRegister(this.instruction.arg1)
             }
             if (typeOfArg(this.instruction.arg1) === cons.NUM || typeOfArg(this.instruction.arg1) === cons.D_NUM) {
-                value = parseInt(parseNum(this.instruction.arg1));
+                value = parseNum(this.instruction.arg1);
             }
-            registers.setRegister(this.instruction.res, parseInt(value));
+            registers.setRegister(this.instruction.res, value);
         }
 
         if (this.instruction.opCode === cons.MVN) {
             let value = 0;
             if (typeOfArg(this.instruction.arg1) === cons.REG) {
-                value = parseInt(registers.getRegister(this.instruction.arg1))
+                value = registers.getRegister(this.instruction.arg1)
             }
             if (typeOfArg(this.instruction.arg1) === cons.NUM || typeOfArg(this.instruction.arg1) === cons.D_NUM) {
-                value = parseInt(parseNum(this.instruction.arg1));
+                value = parseNum(this.instruction.arg1);
             }
             value = ~value;
 
